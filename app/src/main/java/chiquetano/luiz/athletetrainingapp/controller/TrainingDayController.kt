1446642632a2c5eb2cc90af1242d23347783725e
luiz-chiq <chiquetanoluiz@gmail.com>
2023-11-23 23:08:09 -0300

    feat: create: menu_main.xml

--- app/src/main/java/chiquetano/luiz/athletetrainingapp/controller/TrainingDayController.kt
+++ app/src/main/java/chiquetano/luiz/athletetrainingapp/controller/TrainingDayController.kt
@@ -1,4 +1,50 @@
 package chiquetano.luiz.athletetrainingapp.controller
 
-class TrainingDayController {
+
+import android.os.Message
+import chiquetano.luiz.athletetrainingapp.model.TrainingDay
+import chiquetano.luiz.athletetrainingapp.model.TrainingDayDao
+import chiquetano.luiz.athletetrainingapp.model.TrainingDayRtDbFb
+import chiquetano.luiz.athletetrainingapp.view.MainActivity
+
+class TrainingDayController(private val mainActivity: MainActivity) {
+    private val TrainingDayDaoImpl: TrainingDayDao = TrainingDayRtDbFb()
+
+    fun insertTrainingDay(trainingDay: TrainingDay) {
+        Thread {
+            TrainingDayDaoImpl.createTrainingDay(trainingDay)
+        }.start()
+    }
+
+    fun getTrainingDay(id: Int) = TrainingDayDaoImpl.getTrainingDay(id)
+
+    fun getTrainingDays() {
+        Thread {
+            val returnList = TrainingDayDaoImpl.getTrainingDays()
+
+            mainActivity.updateTrainingDayListHandler.apply {
+                sendMessage(Message().apply {
+                    data.putParcelableArray(
+                        "CONTACT_ARRAY",
+                        returnList.toTypedArray()
+                    )
+                })
+            }
+
+        }.start()
+    }
+
+    fun editTrainingDay(trainingDay: TrainingDay){
+        Thread {
+            TrainingDayDaoImpl.updateTrainingDay(trainingDay)
+        }.start()
+    }
+
+    fun removeTrainingDay(trainingDay: TrainingDay){
+        Thread {
+            trainingDay.id?.also {
+                TrainingDayDaoImpl.deleteTrainingDay(it)
+            }
+        }.start()
+    }
 }
